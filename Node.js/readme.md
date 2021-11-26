42737ac4da63910762002f75061a617014120955
LEEJUNGYEON <wjddus7575@naver.com>
2021-11-26 23:32:32 +0900

    Node.js URL로 입력된 값 사용하기

---
+++ Node.js/readme.md
@@ -0,0 +1,14 @@
+# :memo: Node.js
+---
+## 목표 : Node.js Application 만들기
+* JavaScript문법 사용
+* 하나의 파일 안에 html 코드가 있고 그것을 수정하면 1억개의 웹페이지의 내용 한번에 수정가능!!
+* 사용자의 참여유도 가능 : ex) create버튼, data 폴더
+* PHP, JSP, Django, Ruby on Rails가 경쟁관계에 있음
+> WEB Application
+HTML
+WEB browser
+
+> Node.js Application
+JavaScript
+Node.js runtime
---
+++ Node.js/syntax/String.js
@@ -0,0 +1,6 @@
+// 문자열을 표현할땐 "" or ''
+// "문자데이터", + 결합연산자됨
+console.log('1'+'1');
+
+// 글자수 계산
+console.log('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'. length);
